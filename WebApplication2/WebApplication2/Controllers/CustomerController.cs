60a08b2e55a6ab1f816fe28685fc1b83e89941e0
Amar <ajk.86@hotmail.com>
2024-04-16 10:44:54 +0300

    Task

---
+++ WebApplication2/WebApplication2/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer{ Id = "1", Name = "Amar", Address = "QADSIYA" },
+                new Customer{ Id = "2", Name = "Yara", Address = "ADAN" },
+                new Customer{ Id = "3", Name = "OODA", Address = "KAIFAN" }
+                };
+
+            return View(customers);
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+
+
+    }
+}
