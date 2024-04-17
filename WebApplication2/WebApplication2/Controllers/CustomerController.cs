5f49d99c0f30d568113e42214f0e1d5159a9c5ff
Amar <ajk.86@hotmail.com>
2024-04-17 13:32:45 +0300

    Form Submition

--- WebApplication2/WebApplication2/Controllers/CustomerController.cs
+++ WebApplication2/WebApplication2/Controllers/CustomerController.cs
@@ -1,21 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication2.Models;
+using System.Collections.Generic;
 
 namespace WebApplication2.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
-
-        {
-            List<Customer> customers = new List<Customer>()
-            {
+        static List<Customer> customers = new List<Customer>() {
                 new Customer{ Id = "1", Name = "Amar", Address = "QADSIYA" },
                 new Customer{ Id = "2", Name = "Yara", Address = "ADAN" },
                 new Customer{ Id = "3", Name = "OODA", Address = "KAIFAN" }
                 };
+
+        public IActionResult Index()
+        {
             ViewData["data"] = customers;
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateCustomer()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+       
+        public IActionResult CreateCustomer(AddCustomerForm form)
+        {
+            var name = form.Name;
+            var address = form.Address;
+            customers.Add(new Customer { Name = name, Address = address });
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ WebApplication2/WebApplication2/Models/AddCustomerForm.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Models
+{
+    public class AddCustomerForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
