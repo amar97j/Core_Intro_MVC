d516ee33ad628c29e672705eef9396c76ef2cbb7
Amar <ajk.86@hotmail.com>
2024-04-17 08:33:12 +0300

    Add Employee Page

---
+++ WebApplication2/WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee { EmployeeId = 1, FirstName = "AMAR", LastName = "J", Email = "e@example.com", Department = "IT" },
+                new Employee { EmployeeId = 2, FirstName = "MAHA", LastName = "A", Email = "je@example.com", Department = "HR" },
+                new Employee { EmployeeId = 3, FirstName = "HAYA", LastName = "Q", Email = "mln@example.com", Department = "Finance" }
+            };
+
+            return View(employees);
+        }
+    }
+}
